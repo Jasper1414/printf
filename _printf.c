80a0179d0ff87eaf74c02d17c6e92ce57922b41b
cecilia0411 <136025498+cecilia0411@users.noreply.github.com>
2023-08-21 13:54:31 +0200

    Revert "Update _printf.c"

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - function that printf function
+ * _printf - the printf function
  * @format: format
  * Return: printed chars
  */
