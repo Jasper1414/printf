d6465f6cb308084481f6b096b13abd8606d9a211
cecilia0411 <136025498+cecilia0411@users.noreply.github.com>
2023-08-22 10:36:22 +0200

    Update main.h

--- main.h
+++ main.h
@@ -64,9 +64,9 @@ int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
+void print_buffer(char buffer[], int *buff_ind);
 
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
